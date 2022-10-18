852194c63ce092f64dbdd822fbd821189f3f11a0
g00392495@atu.ie <Ballaghlea11111>
2022-10-18 14:57:12 +0100

    added new file called Student

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -1,5 +1,6 @@
 package ie.atu.week5;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class StudentApp {
@@ -8,15 +9,48 @@ public class StudentApp {
         System.out.println("Please enter students name");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
-        System.out.println("You entered " + name);
+        Student Student1 = new Student();
+        Student1.setName(name);
+
 
         System.out.println("Please enter students email");
         String email = input.nextLine();
-        System.out.println("You entered " + email);
+        Student1.setEmail(email);
 
         System.out.println("Please enter students course");
         String course = input.nextLine();
-        System.out.println("You entered " + course);
+        Student1.setCourse(course);
+
+        System.out.println("Please enter the name of another student: ");
+        String name1 = input.nextLine();
+        Student student2 = new Student("" + name1);
+
+        System.out.println("Please enter the email of another student: ");
+        String email1 = input.nextLine();
+        student2.setEmail(email1);
+
+        System.out.println("Please enter students course");
+        String course1 = input.nextLine();
+        student2.setCourse(course1);
+
+        System.out.println("Please enter the name of another student: ");
+        String name2= input.nextLine();
+
+        System.out.println("Please enter the email of another student: ");
+        String email2 = input.nextLine();
+
+        System.out.println("Please enter the course of another student: ");
+        String course2= input.nextLine();
+
+        Student student3 = new Student("" + name2, ""+ email2, ""+course2);
+
+
+        System.out.println("\t Student 1 Details\nStudentName: " +Student1.getName());
+
+
+
+
+
 
     }
 }
